f7535542130cfb19607105027192df8b3b153f87
aidanrucker47 <aidan.rucks529@gmail.com>
2023-11-17 09:49:39 -0800

    Finished Site + css + react router

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Red from "./components/Red";
+import Blue from "./components/Blue";
+import Homepage from "./components/homepage";
+import "./App.css";
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div id="container">
+      <h1 className="welcome">Hello React Router!</h1>
+      <div id="navbar">
+        <Link to="/red">Red</Link> <Link to="/">Home</Link>{" "}
+        <Link to="/blue">Blue</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/Red" element={<Red />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Blue" element={<Blue />} />
+        </Routes>
+      </div>
+      <footer id="navbar">
+        <Link to="/red">Red</Link> <Link to="/">Home</Link>{" "}
+        <Link to="/blue">Blue</Link>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <div className="home">
+      <h1>Homepage</h1>
+    </div>
+  );
+}
